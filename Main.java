6e7667a317c46645d1ccec4422b1b8a094595cb8
rioohoo12 <2381036@unai.edu>
2024-11-17 23:37:00 +0700

    Tambahan Handson9

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+
+import config.Database;
+import repositories.TodoListRepository;
+import repositories.TodoListRepositoryDbImpl;
+import services.TodoListService;
+import services.TodoListServiceImpl;
+import views.TodoListTerminalViewImpl;
+import views.TodoListView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Database database = new Database("todo_si", "root", "", "localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new TodoListRepositoryDbImpl(database);
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalViewImpl(todoListService);
+        todoListView.run();
     }
 }
